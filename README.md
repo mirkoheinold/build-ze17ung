1ef130e0b45097f840f144c11890e3ea5118e49f
Stefan <stadolf@gmail.com>
2017-05-10 23:29:44 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+build-zeitung
+=============
+
+A Symfony project created on May 10, 2017, 10:15 pm.
---
+++ src/AppBundle/Resources/public/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import $ from 'jquery';
+import 'bootstrap';
+import _ from 'lodash';
+
+$(() => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
